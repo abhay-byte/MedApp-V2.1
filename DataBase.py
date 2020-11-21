26d5051e54da1a01b7b1125946b888706806d832
Roshan Alex <66155573+screechingghost@users.noreply.github.com>
2020-11-21 13:03:01 +0530

    Add files via upload

--- DataBase.py
+++ DataBase.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 
 conn = sqlite3.connect('MedApp_database.db')
@@ -5,14 +6,10 @@ conn = sqlite3.connect('MedApp_database.db')
 conn.execute('''CREATE TABLE PATIENTS
          (ID TEXT PRIMARY KEY     NOT NULL,
          NAME           TEXT    NOT NULL,
-         AGE_GENDER            TEXT     NOT NULL,
+         AGE            VARCHAR(4)     NOT NULL,
+         GENDER         VARCHAR(8),
          MODEL_FEATURES        CHAR(50),
          GR         TEXT,
          MR         TEXT,
          BR         TEXT);''')
-
-
-
-
-
 
